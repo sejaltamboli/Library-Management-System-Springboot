d5850b5202a49cb70bd1d5e96098fbe8c6e016b9
sejaltamboli <sejaltamboli06@gmail.com>
2024-02-14 23:31:30 +0530

    MODIFIED: Controller Layer of Author

---
+++ src/main/java/com/infogalaxy/librarymanagementsystemproject/controller/AuthorController.java
@@ -0,0 +1,17 @@
+package com.infogalaxy.librarymanagementsystemproject.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/author/api")
+public class AuthorController {
+
+    @GetMapping("/home")
+    public String author(){
+        return "Welcome to Author API";
+    }
+    
+}
