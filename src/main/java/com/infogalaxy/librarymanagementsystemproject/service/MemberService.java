944f8db64ad9f094ef99f02acfbb91146616bff3
sejaltamboli <sejaltamboli0602@gmail.com>
2024-02-25 18:49:59 +0530

    MODIFIED: Library Member API with Exception Handling

--- src/main/java/com/infogalaxy/librarymanagementsystemproject/service/MemberService.java
+++ src/main/java/com/infogalaxy/librarymanagementsystemproject/service/MemberService.java
@@ -3,12 +3,14 @@ package com.infogalaxy.librarymanagementsystemproject.service;
 
 
 import com.infogalaxy.librarymanagementsystemproject.entity.LibraryMemberEntity;
+import com.infogalaxy.librarymanagementsystemproject.exceptions.MemberNotFoundException;
 import com.infogalaxy.librarymanagementsystemproject.repo.IMemberRepo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MemberService implements IMemberService{
@@ -28,22 +30,38 @@ public class MemberService implements IMemberService{
     }
 
     @Override
-    public LibraryMemberEntity retrieveMemberById(int id) {
-        return iMemberRepo.findById(id).get();
+    public Optional<LibraryMemberEntity> retrieveMemberById(int id) {
+        Optional<LibraryMemberEntity> libraryMemberEntity = iMemberRepo.findById(id);
+        if (libraryMemberEntity.isPresent()){
+            return libraryMemberEntity;
+        } else {
+            throw new MemberNotFoundException("Member with Given Id is not Found in Database");
+        }
+
     }
 
     @Override
     public LibraryMemberEntity updateMemberById(int id, LibraryMemberEntity libraryMemberEntity) {
-        LibraryMemberEntity libraryMemberEntityNew = retrieveMemberById(id);
-        BeanUtils.copyProperties(libraryMemberEntity, libraryMemberEntityNew);
-        return iMemberRepo.save(libraryMemberEntityNew);
+        Optional<LibraryMemberEntity> libraryMemberEntityNew = iMemberRepo.findById(id);
+        if (libraryMemberEntityNew.isPresent()){
+            BeanUtils.copyProperties(libraryMemberEntity, libraryMemberEntityNew);
+            return iMemberRepo.save(libraryMemberEntity);
+        } else {
+            throw new MemberNotFoundException("Member with Given ID is not Found in database");
+        }
+
     }
 
     @Override
     public String deleteMemberById(int id) {
-        LibraryMemberEntity libraryMemberEntity = iMemberRepo.findById(id).get();
-        iMemberRepo.delete(libraryMemberEntity);
-        return "Item Deleted Successfully";
+        Optional<LibraryMemberEntity> libraryMemberEntity = iMemberRepo.findById(id);
+        if (libraryMemberEntity.isPresent()){
+            iMemberRepo.deleteById(id);
+            return "Item Deleted Successfully";
+        } else {
+            throw new MemberNotFoundException("Member with Given ID is not Found in Database");
+        }
+
     }
 
 
