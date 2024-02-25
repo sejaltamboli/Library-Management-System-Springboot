4e7e00a2de0d987d431070c6ad3401a5d5850083
sejaltamboli <sejaltamboli0602@gmail.com>
2024-02-25 20:50:06 +0530

    MODIFIED:Service Interface of Author API

--- src/main/java/com/infogalaxy/librarymanagementsystemproject/service/AuthorService.java
+++ src/main/java/com/infogalaxy/librarymanagementsystemproject/service/AuthorService.java
@@ -1,14 +1,14 @@
-//package com.infogalaxy.librarymanagementsystemproject.service;
-//
-//import com.infogalaxy.librarymanagementsystemproject.entity.AuthorEntity;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//public class AuthorService implements IAuthorService{
-//
-//
-//
-////    @Override
-////    public AuthorEntity createAuthor(AuthorEntity authorEntity) {
-////        return null;
-//   // }
-//}
+package com.infogalaxy.librarymanagementsystemproject.service;
+
+import com.infogalaxy.librarymanagementsystemproject.entity.AuthorEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AuthorService implements IAuthorService{
+
+
+
+//    @Override
+//    public AuthorEntity createAuthor(AuthorEntity authorEntity) {
+//        return null;
+   // }
+}
