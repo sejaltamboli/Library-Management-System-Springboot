73d4af7cd3b6d88a9abf88db067e0294e81f783a
sejaltamboli <sejaltamboli0602@gmail.com>
2024-02-26 16:26:54 +0530

    MODIFIED:Controller Class of Create Book

--- src/main/java/com/infogalaxy/librarymanagementsystemproject/controller/BookController.java
+++ src/main/java/com/infogalaxy/librarymanagementsystemproject/controller/BookController.java
@@ -1,17 +1,30 @@
 package com.infogalaxy.librarymanagementsystemproject.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.infogalaxy.librarymanagementsystemproject.entity.BookEntity;
+import com.infogalaxy.librarymanagementsystemproject.repo.IBookRepo;
+import com.infogalaxy.librarymanagementsystemproject.responses.GlobalResponses;
+import com.infogalaxy.librarymanagementsystemproject.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/book/api")
 public class BookController {
 
+    @Autowired
+    IBookService iBookService;
+
     @GetMapping("/home")
     public String book(){
         return "Welcome to Book API";
+    }
+
+    @PostMapping("/createbook")
+    public ResponseEntity<?> createBook(@RequestBody BookEntity bookEntity) {
+        return new ResponseEntity<>(new GlobalResponses("New Book Created Successfully", HttpStatus.CREATED, iBookService.createBook(bookEntity)), HttpStatus.CREATED);
     }
     
 }
--- src/main/java/com/infogalaxy/librarymanagementsystemproject/entity/BookEntity.java
+++ src/main/java/com/infogalaxy/librarymanagementsystemproject/entity/BookEntity.java
@@ -2,10 +2,12 @@ package com.infogalaxy.librarymanagementsystemproject.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "bookentity")
 public class BookEntity {
 
     @Id
