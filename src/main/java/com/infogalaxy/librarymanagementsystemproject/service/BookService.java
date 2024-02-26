63f3b73c59bbfd4c6f8fe59e8a2f5fab577939c6
sejaltamboli <sejaltamboli0602@gmail.com>
2024-02-26 16:07:41 +0530

    MODIFIED:Service Interface and Service Class of Create Book

--- src/main/java/com/infogalaxy/librarymanagementsystemproject/service/BookService.java
+++ src/main/java/com/infogalaxy/librarymanagementsystemproject/service/BookService.java
@@ -1,8 +1,20 @@
 package com.infogalaxy.librarymanagementsystemproject.service;
 
+import com.infogalaxy.librarymanagementsystemproject.entity.BookEntity;
+import com.infogalaxy.librarymanagementsystemproject.repo.IBookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService implements IBookService{
-    
+
+    @Autowired
+    IBookRepo iBookRepo;
+
+
+    @Override
+    public BookEntity createBook(BookEntity bookEntity) {
+        iBookRepo.save(bookEntity);
+        return bookEntity;
+    }
 }
--- src/main/java/com/infogalaxy/librarymanagementsystemproject/service/IBookService.java
+++ src/main/java/com/infogalaxy/librarymanagementsystemproject/service/IBookService.java
@@ -1,5 +1,8 @@
 package com.infogalaxy.librarymanagementsystemproject.service;
 
+import com.infogalaxy.librarymanagementsystemproject.entity.BookEntity;
+
 public interface IBookService {
-    
+
+    public BookEntity createBook(BookEntity bookEntity);
 }
