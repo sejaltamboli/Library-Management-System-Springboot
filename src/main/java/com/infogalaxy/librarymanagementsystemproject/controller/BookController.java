3da9f73ae2ad6ba50a98649989888e3cb0daa335
sejaltamboli <sejaltamboli0602@gmail.com>
2024-02-26 15:59:38 +0530

    MODIFIED:Book API Datamembers are Declare in Book Entity Class

--- src/main/java/com/infogalaxy/librarymanagementsystemproject/controller/BookController.java
+++ src/main/java/com/infogalaxy/librarymanagementsystemproject/controller/BookController.java
@@ -1,5 +1,17 @@
 package com.infogalaxy.librarymanagementsystemproject.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book/api")
 public class BookController {
+
+    @GetMapping("/home")
+    public String book(){
+        return "Welcome to Book API";
+    }
     
 }
--- src/main/java/com/infogalaxy/librarymanagementsystemproject/entity/BookEntity.java
+++ src/main/java/com/infogalaxy/librarymanagementsystemproject/entity/BookEntity.java
@@ -1,5 +1,24 @@
 package com.infogalaxy.librarymanagementsystemproject.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class BookEntity {
+
+    @Id
+    private int bookid;
+
+    private String booktitle;
+
+    private String bookauthorname;
+
+    private int bookprice;
+
+    private String bookisbnno;
+
+    private int bookpublishyear;
     
 }
